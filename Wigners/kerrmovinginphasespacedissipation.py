58f7c60670563c4e9f69f65873eb97216f99aef0
pfrunzio <71707254+pfrunzio@users.noreply.github.com>
2022-04-02 23:16:11 -0400

    Create kerrmovinginphasespacedissipation.py

---
+++ Wigners/kerrmovinginphasespacedissipation.py
@@ -0,0 +1,93 @@
+# Paola Frunzio
+# 8/10-12/21, 8/20/21
+# gaussian that moves in phase space with Kerr hamiltonian with dissipation terms
+
+# libraries
+import itertools
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import rc
+rc('animation', html='jshtml')
+
+fig = plt.figure()
+plt.title('Gaussian Moving Through Phase Space with Kerr "Cat" Hamiltonian and Dissapation Terms')
+
+fig, ax = plt.subplots()
+xdata, pdata = [], []
+trace, = ax.plot([], [], lw=1)
+
+# inputs
+num = 5 # 8 eventually
+steps = 3
+levels = 100
+
+# defining values
+sigma_x = 1
+sigma_p = 1
+x0 = 3 #x coordinate
+p0 = 0 #y coordinate
+omega = 1
+gamma = 1
+D = 1
+
+# making everything units
+m = 1 #kg
+v_max = 10.0 #m/s
+p_max = m*v_max
+x_max = 10.0 #m
+
+# set up steps
+dx = 2*x_max/pow(2, num)
+dp = 2*p_max/pow(2, num)
+
+# set up plot
+x = np.arange(-x_max, x_max, dx)
+p = np.arange(-p_max, p_max, dp)
+x, p = np.meshgrid(x, p)
+
+T = 10
+N = 50
+Delta_t = T/N
+rho = np.zeros(((len(x), len(p), N)))
+
+def data_gen():
+  for i in itertools.count():
+    t = i/steps
+    yield x0*np.cos(omega*t) + (p0/(m*omega))*np.sin(omega*t), -1*m*omega*x0*np.sin(omega*t) + p0*np.cos(omega*t), i
+
+# normalizing
+gaussian = np.exp(-((pow(x-x0, 2)/(2*(pow(sigma_x, 2))))+(pow(p-p0, 2)/(2*(pow(sigma_p, 2))))))
+A = np.sum(gaussian*dx*dp)
+W = gaussian/A
+
+rho[:, :, 0] = W
+
+nrm = mpl.colors.Normalize(-rho[:, :, 0].max(), rho[:, :, 0].max())
+
+def frame(data):
+  xt, pt, n = data
+  
+  gaussian = np.exp(-((pow(x-xt, 2)/(2*(pow(sigma_x, 2))))+(pow(p-pt, 2)/(2*(pow(sigma_p, 2))))))
+  A = np.sum(gaussian*dx*dp)
+  W = gaussian/A
+
+  rho_dot = (4*pow(x,3)+4*x*pow(p,2)) * -1*((p-pt)/pow(sigma_p,2))*W - (4*pow(p,3)+4*p*pow(x,2)) * -1*((x-xt)/pow(sigma_x,2))*W \
+  # + 2*gamma*p*-1*((p-pt)/pow(sigma_p,2))*W+W #just friction
+  # + D*pow(-1*((p-pt)/pow(sigma_p,2)),2)*W #just diffusion
+  + 2*gamma*p*-1*((p-pt)/pow(sigma_p,2))*W+W + D*pow(-1*((p-pt)/pow(sigma_p,2)),2)*W
+
+  rho[:, :, n+1] = rho[:, :, n] + rho_dot*Delta_t
+
+  nrm = mpl.colors.Normalize(-rho[:, :, n].max(), rho[:, :, n].max())
+  
+  plot = plt.contourf(x, p, rho[:, :, n], levels=levels, cmap="RdBu_r", norm=nrm)
+  return plot
+
+# add colorbar
+plt.colorbar(plt.contourf(x, p, rho[:, :, 0], 100, cmap="RdBu_r", norm=nrm))
+
+ani = animation.FuncAnimation(fig, frame, data_gen, save_count=N-1, blit=False, repeat=True)
+
+ani
